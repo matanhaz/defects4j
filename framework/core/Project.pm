423832f56417298b8050d17b9fabee22f2f13189
amir9979 <amir9979@gmail.com>
2021-06-23 18:50:32 +0300

    Update Project.pm

--- framework/core/Project.pm
+++ framework/core/Project.pm
@@ -1111,7 +1111,7 @@ sub _ant_call {
                 " -q -f $D4J_BUILD_FILE" .
                 " -Dd4j.home=$BASE_DIR" .
                 " -Dd4j.dir.projects=$PROJECTS_DIR" .
-                " -Dbasedir=$self->{prog_root} ${option_str} $target 2>&1";
+                " -Dbasedir=$self->{prog_root} ${option_str} $target 1>nul 2>&1";
     my $log;
     my $ret = Utils::exec_cmd($cmd, "Running ant ($target)", \$log);
 
