fd55e4a8183aae24d5d6f26be1444888db256977
amir9979 <amir9979@gmail.com>
2021-07-20 01:09:39 +0300

    add tracing

---
+++ framework/bug-mining/tracing/Tracer.py
@@ -0,0 +1,162 @@
+import xml.etree.cElementTree as et
+import os
+import sys
+import socket
+import json
+from junitparser.junitparser import Error, Failure, Skipped
+from junitparser import JUnitXml
+from subprocess import Popen
+from jcov_parser import JcovParser
+et.register_namespace('', "http://maven.apache.org/POM/4.0.0")
+et.register_namespace('xsi', "http://www.w3.org/2001/XMLSchema-instance")
+
+
+class TestResult(object):
+    def __init__(self, junit_test, suite_name=None, report_file=None):
+        self.junit_test = junit_test
+        self.classname = junit_test.classname or suite_name
+        self.name = junit_test.name
+        self.time = junit_test.time
+        self.full_name = "{classname}.{name}".format(classname=self.classname, name=self.name)
+        self.report_file = report_file
+        self.outcome = 'pass'
+        if junit_test.result:
+            self.outcome = junit_test.result[0]._tag
+
+    def __repr__(self):
+        return "{full_name}: {outcome}".format(full_name=self.full_name, outcome=self.outcome)
+
+    def is_passed(self):
+        return self.outcome == 'pass'
+
+    def get_observation(self):
+        return 0 if self.is_passed() else 1
+
+    def as_dict(self):
+        return {'_test_name': self.full_name, '_outcome': self.outcome}
+
+
+class Tracer:
+    JCOV_JAR_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "externals", "jcov.jar")
+    path_to_classes_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "externals", "classes")
+    path_to_out_template = os.path.join(os.path.dirname(os.path.realpath(__file__)), "externals", "template")
+    path_to_result_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "results", "result.xml")
+    path_to_tests_details = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_details.json")
+
+    def __init__(self, xml_path):
+        self.classes_dir = None
+        self.command_port = 5552
+        self.agent_port = 5551
+        self.xml_path = xml_path
+        self.element_tree = et.parse(self.xml_path)
+        self.set_junit_formatter()
+        self.element_tree.write(self.xml_path, xml_declaration=True)
+        self.test_results = {}
+
+    def set_junit_formatter(self):
+        junit = list(filter(lambda x: x.tag == 'junit', self.element_tree.iter()))
+        if junit:
+            for j in junit:
+                j.attrib.update({'fork': 'yes'})
+                formatter = list(filter(lambda x: x.tag == 'formatter', j.iter()))
+                if formatter:
+                    formatter = formatter[0]
+                else:
+                    formatter = et.SubElement(j, 'formatter')
+                formatter.attrib.update({'type': 'xml', 'usefile': 'true'})
+                jvmarg = list(filter(lambda x: x.tag == 'jvmarg', j.iter()))
+                if jvmarg:
+                    jvmarg = jvmarg[0]
+                else:
+                    jvmarg = et.SubElement(j, 'jvmarg')
+                arg_line = r'-javaagent:{JCOV_JAR_PATH}=grabber,port={PORT},include_list={CLASSES_FILE},template={OUT_TEMPLATE},type=method'.format(
+                    JCOV_JAR_PATH=Tracer.JCOV_JAR_PATH, PORT=self.agent_port, CLASSES_FILE=Tracer.path_to_classes_file, OUT_TEMPLATE=Tracer.path_to_out_template)
+                jvmarg.attrib.update({'value': arg_line})
+
+    def get_classes_path(self):
+        all_classes = {os.path.dirname(self.xml_path)}
+        for root, dirs, files in os.walk(os.path.dirname(self.xml_path)):
+            for f in files:
+                if f.endswith('class'):
+                    all_classes.add(root)
+                    break
+        return all_classes
+
+    def template_creator_cmd_line(self):
+        cmd_line = [os.path.join(os.environ['JAVA_HOME'], "bin\java.exe"), '-Xms2g', '-jar', Tracer.JCOV_JAR_PATH, 'tmplgen', '-verbose']
+        cmd_line.extend(['-t', Tracer.path_to_out_template])
+        cmd_line.extend(['-c', Tracer.path_to_classes_file])
+        cmd_line.extend(['-type', 'method'])
+        cmd_line.extend(self.get_classes_path())
+        return cmd_line
+
+    def grabber_cmd_line(self):
+            cmd_line = [os.path.join(os.environ['JAVA_HOME'], "bin\java.exe"), '-Xms2g', '-jar', Tracer.JCOV_JAR_PATH, 'grabber', '-vv', '-port', self.agent_port, '-command_port', self.command_port]
+            cmd_line.extend(['-t', Tracer.path_to_out_template])
+            cmd_line.extend(['-o', Tracer.path_to_result_file])
+            return list(map(str, cmd_line))
+
+    def check_if_grabber_is_on(self):
+        import time
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            time.sleep(5)
+            s.connect(('127.0.0.1', int(self.command_port)))
+            s.close()
+            return True
+        except:
+            return False
+
+    def execute_jcov_process(self):
+        Popen(self.template_creator_cmd_line()).communicate()
+        for path in [self.path_to_classes_file, self.path_to_out_template]:
+            if path:
+                with open(path) as f:
+                    assert f.read(), "{0} is empty".format(path)
+        print(self.grabber_cmd_line())
+        p = Popen(self.grabber_cmd_line())
+        assert p.poll() is None
+        assert self.check_if_grabber_is_on()
+
+    def stop_grabber(self):
+        Popen(["java", "-jar", Tracer.JCOV_JAR_PATH, "grabberManager", "-save", '-command_port', str(self.command_port)]).communicate()
+        Popen(["java", "-jar", Tracer.JCOV_JAR_PATH, "grabberManager", "-stop", '-command_port', str(self.command_port)]).communicate()
+        traces = list(JcovParser(os.path.dirname(Tracer.path_to_result_file), True, True).parse(False))[0].split_to_subtraces()
+        self.observe_tests()
+        relevant_traces = list(filter(lambda t: t in self.test_results, traces))
+        tests_details = []
+        for t in relevant_traces:
+            tests_details.append((t, traces[t].get_trace(), 0 if self.test_results[t].outcome == 'pass' else 1))
+        with open(Tracer.path_to_tests_details, "w") as f:
+            json.dump(tests_details, f)
+
+    def get_xml_files(self):
+        for root, _, files in os.walk(os.path.dirname(self.xml_path)):
+            for name in files:
+                if name.endswith('.xml'):
+                    yield os.path.join(root, name)
+
+    def observe_tests(self, save_to=None):
+        self.test_results = {}
+        for report in self.get_xml_files():
+            try:
+                suite = JUnitXml.fromfile(report)
+                for case in suite:
+                    test = TestResult(case, suite.name, report)
+                    self.test_results[test.full_name.lower()] = test
+            except Exception as e:
+                print(e, report)
+                pass
+        if save_to:
+            with open(save_to, "w") as f:
+                json.dump(list(map(lambda x: self.test_results[x].as_dict(), self.test_results)), f)
+        return self.test_results
+
+
+if __name__ == '__main__':
+    t = Tracer(os.path.abspath(sys.argv[1]))
+    if len(sys.argv) == 3:
+        if sys.argv[-1] == 'start':
+            t.execute_jcov_process()
+        else:
+            t.stop_grabber()
---
+++ framework/bug-mining/tracing/jcov_parser.py
@@ -0,0 +1,117 @@
+import functools
+import os
+import gc
+import shutil
+import xml.etree.cElementTree as et
+from functools import reduce
+from trace_information import Signature, TraceElement, Trace
+
+
+class JcovParser(object):
+    CLOSER = "/>"
+    METH = "<meth"
+    METHENTER = "<meth"
+    CSV_HEADER = ["component", "hit_count"]
+
+    def __init__(self, xml_folder_dir, instrument_only_methods=True, short_type=True):
+        self.target_dir = xml_folder_dir
+        self.jcov_files = list(filter(os.path.exists, map(lambda name: os.path.join(self.target_dir, name),
+                              filter(lambda name: name.endswith('.xml'), os.listdir(self.target_dir)))))
+        self.instrument_only_methods = instrument_only_methods
+        self.prefixes = set()
+        if self.instrument_only_methods:
+            self.prefixes.add(JcovParser.METH)
+        self.method_name_by_id = self._get_method_ids(short_type)
+        self.lines_to_read = self._get_methods_lines()
+
+    def parse(self, delete_dir_when_finished=False):
+        for jcov_file in self.jcov_files:
+            test_name = os.path.splitext(os.path.basename(jcov_file))[0].lower()
+            yield self._parse_jcov_file(jcov_file, test_name)
+        if delete_dir_when_finished:
+            shutil.rmtree(self.target_dir)
+
+    def _parse_jcov_file(self, jcov_file, test_name):
+        gc.collect()
+        trace = self._get_trace_for_file(jcov_file)
+        list(map(lambda element: element.set_previous_method(self.method_name_by_id), trace.values()))
+        return Trace(test_name, trace)
+
+    def _get_trace_for_file(self, jcov_file):
+        trace = {}
+        for method in self._get_lines_by_inds(jcov_file):
+            prefix = list(filter(lambda prefix: method.startswith(prefix), self.prefixes))[0]
+            data = dict(list(map(lambda val: val.split('='),
+                            method[len(prefix) + 1:-len(JcovParser.CLOSER)].replace('"', "").split())))
+            trace_element = TraceElement(data, self.method_name_by_id)
+            if trace_element.have_count():
+                assert trace_element.id not in trace
+                trace[trace_element.id] = trace_element
+        return trace
+
+    def _get_lines_by_inds(self, file_path):
+        with open(file_path) as f:
+            enumerator_next_ind = 0
+            for ind in self.lines_to_read:
+                list(map(functools.partial(next, f), range(enumerator_next_ind, ind)))
+                enumerator_next_ind = ind + 1
+                yield next(f).strip()
+
+    def _get_methods_lines(self):
+        with open(self.jcov_files[0]) as f:
+            return list(map(lambda line: line[0], filter(lambda line: any(list(map(lambda prefix: prefix in line[1], self.prefixes))), filter(lambda line: JcovParser.CLOSER in line[1], enumerate(f.readlines())))))
+
+    @staticmethod
+    def get_children_by_name(element, name):
+        return list(filter(lambda e: e.tag.endswith(name), element.getchildren()))
+
+    @staticmethod
+    def get_elements_by_path(root, path):
+        elements = [([], root)]
+        for name in path:
+            elements = reduce(list.__add__,
+                              list(map(lambda elem: list(map(lambda child: (elem[0] + [child], child),
+                                                   JcovParser.get_children_by_name(elem[1], name))), elements)), [])
+        return elements
+
+    def _get_method_ids(self, short_type):
+        root = et.parse(self.jcov_files[0]).getroot()
+        method_ids = {}
+        for method_path, method in JcovParser.get_elements_by_path(root, ['package', 'class', 'meth']):
+            package_name, class_name, method_name = list(map(lambda elem: elem.attrib['name'], method_path))
+            if method_name == '<init>':
+                method_name = class_name
+            elif method_name == '<clinit>':
+                method_name = class_name + "_" + "init"
+            method_name = ".".join([package_name, class_name, method_name]) + "({0})".format(
+                Signature(method.attrib['vmsig'], short_type).args)
+            if self.instrument_only_methods:
+                method_ids[int(method.attrib['id'])] = method_name
+                method_ids[int(method.attrib['extra_slots'])] = method_name
+            else:
+                # id = JcovParser.get_elements_by_path(method, ['bl', 'methenter'])[0][1].attrib['id']
+                # extra_slot = JcovParser.get_elements_by_path(method, ['bl', 'methenter'])[0][1].attrib['extra_slots']
+                method_ids.update(self._get_method_blocks_ids(method, method_name))
+        return method_ids
+
+    def _get_method_blocks_ids(self, method_et, method_name):
+        ids = {}
+        for et in method_et.getchildren():
+            id = et.attrib.get("id")
+            if id:
+                prefix = et.tag.split("}")[1]
+                self.prefixes.add("<" + prefix)
+                ids[int(id)] = method_name + "." + prefix
+            else:
+                ids.update(self._get_method_blocks_ids(et, method_name))
+        return ids
+
+
+def block_to_comps(block):
+    splitted = block.split(".")
+    package_name = ".".join(splitted[:-3])
+    class_name = ".".join(splitted[:-2])
+    function_name = ".".join(splitted[:-1])
+    block_name = ".".join(splitted)
+    return [package_name, class_name, function_name, block_name]
+
