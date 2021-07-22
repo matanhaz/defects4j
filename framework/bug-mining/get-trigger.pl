7c1861df1881865e455721e6d6231e042e929f45
amir9979 <amir9979@gmail.com>
2021-07-22 18:47:54 +0300

    Update get-trigger.pl

--- framework/bug-mining/get-trigger.pl
+++ framework/bug-mining/get-trigger.pl
@@ -250,7 +250,7 @@ foreach my $bid (@bids) {
 
 $dbh_trigger->disconnect();
 $dbh_revs->disconnect();
-system("rm -rf $TMP_DIR");
+# system("rm -rf $TMP_DIR");
 
 #
 # Get bug ids from TAB_REV_PAIRS
