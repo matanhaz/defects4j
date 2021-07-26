c72e26f5117ef5db3ad08e94c96da7cd64580e48
amir9979 <amir9979@gmail.com>
2021-07-26 23:59:34 +0300

    Update get-trigger.pl

--- framework/bug-mining/get-trigger.pl
+++ framework/bug-mining/get-trigger.pl
@@ -393,7 +393,7 @@ sub _trace_tests {
 	system("cd tracing && python Tracer.py ${root} grabber 2>&1 &");
 	sleep(20);
     # $project->run_tests($TESTS_FILE) or die;
-    $project->_ant_call_comp("test", "-DOUTFILE=$TESTS_FILE")  or die;
+    $project->_ant_call_comp("test", "-keep-going")  or die;
 	system(" cd tracing && python Tracer.py ${root} stop 2>&1");
 }
 
