34152acdd2afec85395755c020fb82b0b9428a89
Amir Elmishali <amir9979@gmail.com>
2021-08-04 14:46:28 +0300

    Update Tracer.py

--- framework/bug-mining/tracing/Tracer.py
+++ framework/bug-mining/tracing/Tracer.py
@@ -153,7 +153,7 @@ class Tracer:
         print(relevant_traces)
         tests_details = []
         for t in relevant_traces:
-            tests_details.append((t, traces[t].get_trace(), 0 if self.test_results[t.split('(')[0].lower()].outcome == 'pass' else 1))
+            tests_details.append((t, traces[t].get_trace(), self.test_results[t.split('(')[0].lower()].get_observation()))
         tests_details = list(filter(lambda x: x[1], tests_details))
         tests_names = set(list(map(lambda x: x[0], tests_details)) + list(map(lambda x: x[0].lower(), tests_details)))
         fail_components = reduce(set.__or__, list(map(lambda x: set(x[1]), filter(lambda x: x[2] == 1, tests_details))), set())
