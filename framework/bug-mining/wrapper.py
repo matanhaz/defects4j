e20a33acec2112537cd6245529fd70c20e4d12e2
amir9979 <amir9979@gmail.com>
2021-11-18 15:22:50 +0200

    Update wrapper.py

--- framework/bug-mining/wrapper.py
+++ framework/bug-mining/wrapper.py
@@ -290,7 +290,7 @@ projects = {'distributedlog': ('https://github.com/apache/distributedlog', 'DL')
 
 
 def get_cmds(p, working_dir, ind):
-    getters = {'p': projects[p][1].title(), 'r': projects[p][0], 'n': p, 'g': 'jira', 't': projects[p][1], 'e': '"/({0}-\d+)/mi"'.format(projects[p][1]), 'w': working_dir, 'i': ind, 'a': f"{working_dir}//project_repos//{p}", 'b': f"{working_dir}//framework//projects//{projects[p][1].title()}//active-bugs.csv"}
+    getters = {'p': projects[p][1].title(), 'r': projects[p][0], 'n': p, 'g': 'jira', 't': projects[p][1], 'e': '"/({0}-\d+)/mi"'.format(projects[p][1]), 'w': working_dir, 'i': ind, 'a': f"{working_dir}//project_repos//{p}.git", 'b': f"{working_dir}//framework//projects//{projects[p][1].title()}//active-bugs.csv"}
     files_cmds = [(['./initialize-project-and-collect-issues.pl'], ['p', 'n', 'r', 'g', 't', 'e', 'w']), (['python', './extractor.py'], ['a', 'w', 'b']), (['./initialize-revisions.pl'], ['p', 'w', 'i']), (['./analyze-project.pl'], ['p', 'w', 'g', 't', 'i']), (['./get-trigger.pl'], ['p', 'w']), (['./get-metadata.pl'], ['p', 'w'])]
     for f in files_cmds:
         yield f[0] + reduce(list.__add__, list(map(lambda x: [f'-{x}', getters[x]], f[1])), [])
