fb3cf9cdc9d6043711334ba2274c48e6069e0cf0
amir9979 <amir9979@gmail.com>
2021-12-20 14:04:22 +0200

    fix layout

---
+++ framework/bug-mining/layout.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import csv
+
+def layout(repo_path, out_file):
+	java = set()
+	tests = set()
+	for root, dirs, files in os.walk(repo_path, topdown=False):
+		for name in filter(lambda x: x.endswith('.java'), files):
+			if 'test' in root:
+				tests.add(root[len(repo_path) + 1:])
+			else:
+				java.add(root[len(repo_path) + 1:])
+	reduced_java = set()
+	s_j = min(list(map(lambda x: len(x), java)))
+	min_java_name = list(filter(lambda x: len(x) == s_j, java))[0]
+	reduced_tests = set()
+	s_t = min(list(map(lambda x: len(x), tests)))
+	min_test_name = list(filter(lambda x: len(x) == s_t, tests))[0]
+	for name in java:
+		if os.path.dirname(name) in java:
+			continue
+		if name != min_java_name and name[:s_j] == min_java_name:
+			continue
+		reduced_java.add(name)
+	for name in tests:
+		if os.path.dirname(name) in tests:
+			continue
+		if name != min_test_name and name[:s_t] == min_test_name:
+			continue
+		reduced_tests.add(name)
+	lines = [sorted(reduced_java, key=lambda x: len(x))[0], sorted(reduced_tests, key=lambda x: len(x))[0]]
+	with open(out_file, 'w') as f:
+		f.writelines(map(lambda x: x + '\n',lines))
+
+
+if __name__ == '__main__':
+	repo_dir = sys.argv[1]
+	out_file = sys.argv[2]
+	layout(repo_dir, out_file)
