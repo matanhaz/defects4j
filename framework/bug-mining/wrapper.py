96757a928bd0a9d1816c1c7a74f6236641a2d2b1
amir9979 <amir9979@gmail.com>
2022-01-31 23:50:02 +0200

    Update wrapper.py

--- framework/bug-mining/wrapper.py
+++ framework/bug-mining/wrapper.py
@@ -525,7 +525,7 @@ class Reproducer:
         # sanity trace
         os.system(f"cd tracing && python Tracer.py {repo.working_dir} sanity {os.path.join(self.projects_dir, self.pid)} formatter 2>&1")
         os.system(f"cd tracing && python Tracer.py {repo.working_dir} sanity {os.path.join(self.projects_dir, self.pid)} template 2>&1")
-        # os.system(f"cd tracing && python Tracer.py {repo.working_dir} sanity {os.path.join(self.projects_dir, self.pid)} grabber 2>&1 &")
+        os.system(f"cd tracing && python Tracer.py {repo.working_dir} sanity {os.path.join(self.projects_dir, self.pid)} grabber 2>&1 &")
         time.sleep(20)
         # os.system(f"cd {repo.working_dir} && ant -q  -Dbuild.compiler=javac1.8  -keep-going test 2>&1")
         os.system(f"cd tracing && python Tracer.py {repo.working_dir} sanity {os.path.join(self.projects_dir, self.pid)} stop 2>&1")
