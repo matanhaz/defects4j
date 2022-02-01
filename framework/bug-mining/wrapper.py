819099ebb9a739458747b53505944255263e8854
amir9979 <amir9979@gmail.com>
2022-02-01 17:20:49 +0200

    Update wrapper.py

--- framework/bug-mining/wrapper.py
+++ framework/bug-mining/wrapper.py
@@ -490,10 +490,10 @@ class Reproducer:
         os.system(
             f"cd {repo.working_dir} && ant -q  -Dbuild.compiler=javac1.8  compile-tests 2>&1 > {os.path.join(self.work_dir, 'compile_tests_trigger_log.log')}")
 
-        fix(get_candidates(os.path.join(self.work_dir, 'compile_tests_trigger_log.log'),
-                           repo.working_dir.split('pl')[0]) + collect_failed_tests(
-            os.path.join(os.path.dirname(os.path.join(self.work_dir, 'compile_tests_trigger_log.log')),
-                         'failing_tests.log'), repo.working_dir.split('pl')[0]))
+        candidates = get_candidates(os.path.join(self.work_dir, 'compile_tests_trigger_log.log'),
+                                    repo.working_dir.split('pl')[0])
+        failed_tests = collect_failed_tests(os.path.join(self.work_dir, 'failing_tests.log'), repo.working_dir.split('pl')[0])
+        fix(candidates + failed_tests)
         os.system(
             f"cd {repo.working_dir} && ant -q  -Dbuild.compiler=javac1.8  compile-tests 2>&1 > {os.path.join(self.work_dir, 'compile_tests_trigger_log.log')}")
 
